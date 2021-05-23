4930e803afff95937474ea7c4a284a80b79296d9
mihiraj779 <84088001+mihiraj779@users.noreply.github.com>
2021-05-23 12:39:24 +0530

    Add files via upload

---
+++ Task2_TrafficLight_Client .cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include<ctype.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+ 
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+ 
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    int t;
+ 
+    char buffer[1024];
+    if (argc < 3)
+    {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    cout<<"========Client========== \n";
+    
+    read(sockfd,buffer,1024);
+    printf("%s",buffer);
+    cin>>t;
+    write(sockfd,&t,sizeof(int));   
+    for(int i=1;i>0;i++)
+    {
+        int a;
+        if(i<=t)
+        {
+            cout<<"Input line "<<i<<": ";
+            for(int i=0;i<8;i++)
+            {
+                cin>>a;
+                write(sockfd,&a,sizeof(int));
+            }
+        } 
+        else{
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
+ 
+
+
+
